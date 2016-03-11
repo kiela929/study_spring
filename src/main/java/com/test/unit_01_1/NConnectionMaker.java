3557902b7a2d5672b084fcb54111e26655da7556
kiela929 <kiela929@naver.com>
2016-03-11 23:44:17 +0900

    unit_01-7 제어의역전-오브젝트팩토리만들기(2) NConnectionMaker.java 내용 채워넣기

--- src/main/java/com/test/unit_01_1/NConnectionMaker.java
+++ src/main/java/com/test/unit_01_1/NConnectionMaker.java
@@ -1,15 +1,22 @@
 package com.test.unit_01_1;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class NConnectionMaker implements ConnectionMaker {
 
 	@Override
 	public Connection makeConnection() throws ClassNotFoundException,
-			SQLException {
-		// N사의 독자적인 connection 방법을 구현 
-		return null;
+	SQLException {
+		System.out.println("N사의 connection입니다.");
+
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection c= DriverManager.getConnection(
+				"jdbc:oracle:thin:@localhost:1521:orcl","scott","tiger");
+
+
+		return c;
 	}
 
 }
