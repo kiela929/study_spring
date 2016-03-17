64cb3c541100d56327d0a7673bbe5d18d9981b93
kiela929 <kiela929@naver.com>
2016-03-17 11:46:03 +0900

    unit_01-11 connection을 직접생성이 아닌 DataSource 인터페이스 활용 connection을 이전까지 계속해서 사용해왔던 connectionMaker가 아닌 DataSource 인터페이스의 getConnection() 메서드를 활용하였음. 주의/참고: 1) applicationContext의 xml사용방식을 잠시 DaoFactory클래스로 돌려놓고 테스트하였음. 2) pom.xml에서 DataSourece 인터페이스를 사용하기 위한 라이브러리 추가함. org.springframework.jdbc-4.2.5.RELEASE.jar 추가.

--- src/main/java/com/test/unit_01_1/DaoFactory.java
+++ src/main/java/com/test/unit_01_1/DaoFactory.java
@@ -1,16 +1,35 @@
 package com.test.unit_01_1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 
 @Configuration //애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정정보라는 표시이다. 
 public class DaoFactory {
 	
+	@Bean
+	public DataSource dataSource(){
+		SimpleDriverDataSource dataSource=new SimpleDriverDataSource();
+
+		dataSource.setDriverClass(oracle.jdbc.driver.OracleDriver.class);
+		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+		dataSource.setUsername("scott");
+		dataSource.setPassword("tiger");
+		
+		return dataSource;
+		
+	}
 	@Bean //오브젝트 생성을 담당하는 IoC용 메소드라는 표시이다. 
 	public UserDao userDao(){
 		UserDao userDao =new UserDao();
-		userDao.setConnectionMaker(connectionMaker());
+		userDao.setDataSource(dataSource());
+//		userDao.setConnectionMaker(connectionMaker());
 		return userDao;
 	}
 	
--- src/main/java/com/test/unit_01_1/UserDao.java
+++ src/main/java/com/test/unit_01_1/UserDao.java
@@ -6,28 +6,34 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
 public class UserDao {
-	private ConnectionMaker connectionMaker;
+/*	private ConnectionMaker connectionMaker;
 	
-	/*public UserDao(ConnectionMaker connectionMaker){
+	public UserDao(ConnectionMaker connectionMaker){
 		this.connectionMaker=connectionMaker;
 		//userDao를 사용하려면 생성자를 이용해 N사든 D사든 원하는 것으로 만들면 
 		//사용 할 수 있다. 즉, 현재 이 클래스는 어느 회사의 connection을 쓸지에는 관심 없다. 
 		//단지 connection을 쓴다는 것 자체에만 관심있다는 것!! 
 		//어떤것을 사용할지는 클라이언트에게 맡기는 것! 이렇게 해야 의존관계가 제대로 성립된다. 
 
-	}*/
+	}
 	
 	public void setConnectionMaker(ConnectionMaker connectionMaker){
 		this.connectionMaker=connectionMaker;
 		// 생성자를 사용하는 것 보다 이렇게 수정자 메서드를 이용하는 것이 훨씬 더 편리한 방법이다. 
 		//생성자는 한번만 사용가능하지만, 메서드는 사용횟수에 제한을 받지않는다.
-	}
+	}*/
 	
+	private DataSource dataSource;
+	
+	public void setDataSource(DataSource dataSource){
+		this.dataSource=dataSource;
+	}
 	public void add(User user) throws ClassNotFoundException, SQLException{
 		
-		Connection c = connectionMaker.makeConnection(); //인터페이스에 정의된 메소드를 사용하므로 클래스가 바뀐다고 해도 메소드 이름이 변경될 걱정은 없다.
-		
+		Connection c = dataSource.getConnection();
 		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
 		ps.setString(1, user.getId());
 		ps.setString(2, user.getName());
@@ -43,8 +49,7 @@ public class UserDao {
 	
 	public User get(String id) throws ClassNotFoundException, SQLException{
 
-		Connection c = connectionMaker.makeConnection(); //인터페이스에 정의된 메소드를 사용하므로 클래스가 바뀐다고 해도 메소드 이름이 변경될 걱정은 없다.
-			
+		Connection c = dataSource.getConnection();
 		
 		PreparedStatement ps = c.prepareStatement(
 				"select * from users where id=?");
