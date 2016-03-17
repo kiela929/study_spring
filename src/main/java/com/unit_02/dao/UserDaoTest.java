d4e72cb61e0fcda0df8b6e254127993ac82cef99
kiela929 <kiela929@naver.com>
2016-03-17 21:50:30 +0900

    unit_02-1 기존의 dao를 Junit으로 테스트진행

--- src/main/java/com/unit_02/dao/UserDaoTest.java
+++ src/main/java/com/unit_02/dao/UserDaoTest.java
@@ -1,10 +1,13 @@
 package com.unit_02.dao;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.sql.SQLException;
 
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import com.unit_02.domain.User;
@@ -12,42 +15,34 @@ import com.unit_02.domain.User;
 
 
 public class UserDaoTest {
-
-	public static void main(String[] args) throws ClassNotFoundException, SQLException{
-
-				ApplicationContext context =
-						new GenericXmlApplicationContext("com/test/unit_01_1/applicationContext.xml");
-		//applicationContext에서 DaoFactory 대신 xml을 이용하여 빈을 관리하는 방법이다. 
-
-
-		UserDao dao= context.getBean("userDao",UserDao.class);
-
+	
+	
+	@Test
+	public void addAndGet() throws SQLException, ClassNotFoundException{
+		
+		ApplicationContext context = new GenericXmlApplicationContext("com/unit_02/dao/applicationContext.xml");
+		
+		UserDao dao= context.getBean("userDao",UserDao.class); 
+		
 		User user= new User();
-		user.setId("phj929");
-		user.setName("Han");
-		user.setPassword("enjoy");
+		user.setId("moon");
+		user.setName("김달빛");
+		user.setPassword("moonlight");
+		
 		dao.add(user);
 
-		System.out.println(user.getId()+"등록성공");
 		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
-
-		System.out.println(user2.getPassword());
-
-		System.out.println(user2.getId()+"조회 성공");
-
-
-		/**
-		 * 결과 화면
-		 * 
-		 * D사의 connection입니다.
-		 *	phj929등록성공
-		 *	D사의 connection입니다.
-		 *	Han
-		 *	enjoy
-		 *	phj929조회 성공
-		 */
-
+		
+		assertThat(user2.getName(),is(user.getName()));
+		assertThat(user2.getPassword(),is(user.getPassword()));
+		
 	}
+	
+	public static void main(String[] args) {
+		JUnitCore.main("com.unit_02.dao.UserDaoTest");
+		//main에서 junit test를 진행해준다. 
+	}
+
+	
 
 }
