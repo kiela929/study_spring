a813119b5af5b64e9154f970d443f1a448834104
kiela929 <kiela929@naver.com>
2016-03-24 17:29:46 +0900

    unit_02-3 getCount함수에 대한 일관적인 테스트하기

--- src/main/java/com/unit_02/dao/UserDaoTest.java
+++ src/main/java/com/unit_02/dao/UserDaoTest.java
@@ -40,6 +40,30 @@ public class UserDaoTest {
 		
 	}
 	
+	@Test
+	public void count()throws SQLException, ClassNotFoundException{
+		ApplicationContext context = 
+				new GenericXmlApplicationContext("com/unit_02/dao/applicationContext.xml");
+		
+		UserDao dao= context.getBean("userDao",UserDao.class);
+		User user1 = new User("gyumee","박성철","springno1");
+		User user2 = new User("leegw700","이길원","springno2");
+		User user3 = new User("bumjin","박범진","springno3");
+		
+		dao.deleteAll();
+		assertThat(dao.getCount(),is(0));
+		
+		dao.add(user1);
+		assertThat(dao.getCount(),is(1));
+		
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		
+		dao.add(user3);
+		assertThat(dao.getCount(), is(3));
+		
+	}
+	
 	public static void main(String[] args) {
 		JUnitCore.main("com.unit_02.dao.UserDaoTest");
 		//main에서 junit test를 진행해준다. 
