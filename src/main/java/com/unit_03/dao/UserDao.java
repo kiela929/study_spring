4ca276a5c107c8fe166d3bf8350f94a8625f2783
kiela929 <kiela929@naver.com>
2016-04-03 00:47:37 +0900

    unit_03-2)-(3.3.2) 중첩클래스와 익명내부클래스

--- src/main/java/com/unit_03/dao/UserDao.java
+++ src/main/java/com/unit_03/dao/UserDao.java
@@ -12,72 +12,100 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import com.unit_03.domain.User;
 
 public class UserDao {
-	
+
 	private DataSource dataSource;
-	
+
 	public void setDataSource(DataSource dataSource){
 		this.dataSource=dataSource;
 	}
-	public void add(User user) throws SQLException{
-		StatementStrategy st= new AddStatement(user);
-		jdbcContextWithStatementStrategy(st);
+	
+	public void add(final User user) throws SQLException{ //익명내부클래스로 바꾸기!!
+
+
+		jdbcContextWithStatementStrategy(
+				new StatementStrategy() { //익명 내부 클래스는 구현하는 인터페이스를 생성자처럼 이용해서 오브젝트로 만든다. 
+					public PreparedStatement makePreparedStatement(Connection c)
+							throws SQLException {
+
+						PreparedStatement ps =
+								c.prepareStatement("insert into users(id,name,password)values(?,?,?)");
+
+						ps.setString(1, user.getId());
+						ps.setString(2, user.getName());
+						ps.setString(3, user.getPassword());
+
+						return ps;
+					}
+				});
+
 	}
-	
-	
-	
+
+
+
 	public User get(String id) throws ClassNotFoundException, SQLException{
 
 		Connection c = dataSource.getConnection();
-		
+
 		PreparedStatement ps = c.prepareStatement(
 				"select * from users where id=?");
 		ps.setString(1,id);
-		
+
 		ResultSet rs=ps.executeQuery();
-		
+
 		User user =null;
-		
+
 		if(rs.next()){
 			//만일 데이터가 없을 경우 SQLException이 일어나는 것을 방지하는 것.
-		user = new User();
-		user.setId(rs.getString("id"));
-		user.setName(rs.getString("name"));
-		user.setPassword(rs.getString("password"));
+			user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));
 		}
-		
+
 		rs.close();
 		ps.close();
 		c.close();
-		
+
 		if(user==null) throw new EmptyResultDataAccessException(1);
 		//데이터가 없을 경우 다음과 같은 익셉션을 던져줘~
-		
+
 		return user;
 	}
+
 	
-	public void deleteAll() throws SQLException{
-		StatementStrategy st = new DeleteAllStatement(); //전략 클래스의 오브젝트 생성
-		jdbcContextWithStatementStrategy(st);//컨텍스트 호출! 전략오브젝트를 전달함.
-	
+	public void deleteAll() throws SQLException{ //익명내부클래스로 바꾸기!!
+
+		jdbcContextWithStatementStrategy(
+				new StatementStrategy() { //StatementStrategy는 원래 구현해야할 인터페이스이름.
+					//그러나 익명 내부 클래스는 구현하는 인터페이스를 생성자처럼 사용해서 오브젝트로 만든다!! 
+
+					public PreparedStatement makePreparedStatement(Connection c)
+							throws SQLException {
+
+						return c.prepareStatement("delete from users");
+					}
+
+				});
+
 	}
-	
+
 	public int getCount()throws SQLException{
-		
+
 		Connection c = dataSource.getConnection();
-		
+
 		PreparedStatement ps = c.prepareStatement("select count(*) from users");
-		
+
 		ResultSet rs=ps.executeQuery();
 		rs.next();
 		int count = rs.getInt(1);
-		
+
 		rs.close();
 		ps.close();
 		c.close();
-		
+
 		return count;
 	}
-	
+
 	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws
 	SQLException{
 		Connection c = null;
@@ -89,7 +117,7 @@ public class UserDao {
 
 		} catch (SQLException e) {
 			throw e;
-			
+
 		}finally{
 			if(ps !=null){
 				try{
@@ -97,12 +125,12 @@ public class UserDao {
 				} catch(SQLException e){
 				}
 			}
-		if(c!=null){
-			try{
-				ps.close();
-			} catch(SQLException e){
+			if(c!=null){
+				try{
+					ps.close();
+				} catch(SQLException e){
+				}
 			}
-		}
 		}
 	}
 
